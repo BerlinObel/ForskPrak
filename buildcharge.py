ef4648c0ca62421a31ba1267b2bdcbcc69db653e
BerlinObel <oscarobel@gmail.com>
2022-09-29 14:56:57 +0200

    charge buil

--- buildcharge.py
+++ buildcharge.py
@@ -17,7 +17,7 @@ import iteround, itertools
 from sklearn.model_selection import ParameterGrid
 import sys
 
-def score(id,edges,rnd_symbol,symbols,bond_score,het_score,hom_score):
+def score(id,edges,rnd_symbol,symbols,bond_score,het_score,hom_score, rnd_charge, charges, charge_score):
     score = 0
     for edge in edges[np.isin(edges[:,1],id)]:
         score += bond_score
@@ -25,9 +25,15 @@ def score(id,edges,rnd_symbol,symbols,bond_score,het_score,hom_score):
             score += het_score
         elif rnd_symbol == symbols[edge[0]]:
             score += hom_score
+        if charges[edge[0]] == 'H':
+            score += 0
+        elif int(rnd_charge)+int(charges[edge[0]]) == 0:
+            score += charge_score
+        elif int(rnd_charge)+int(charges[edge[0]]) != 0:
+            score += 0
     return score
 
-def grid_particle(elements,starting_size,n_atoms_added,n_hops,bond_score,het_score,hom_score,rnd_seed):
+def grid_particle(elements,starting_size,n_atoms_added,n_hops,bond_score,het_score,hom_score,rnd_seed,charge_score,n_charges):
 
     # set random seed
     np.random.seed(rnd_seed)
@@ -37,10 +43,14 @@ def grid_particle(elements,starting_size,n_atoms_added,n_hops,bond_score,het_sco
     lc = 3.8
     atoms = FaceCenteredCubic('H', surfaces, layers, latticeconstant=lc)
 
+    #charge stuff
+    charges = np.delete(np.linspace(-n_charges,n_charges,2*n_charges+1),n_charges)
     # neighbor library, symbol list and possible bonds
     edge_lib = np.array(neighbor_list('ij',atoms,cutoff=lc*0.9)).transpose()
     symbol_lib = atoms.get_chemical_symbols()
+    charge_lib = atoms.get_chemical_symbols()
     bonds = np.array([set(a) for a in list(itertools.combinations_with_replacement(elements, 2))])
+
 
     # find ids of initial particle by distance from center
     pos = atoms.get_positions()
@@ -51,22 +61,27 @@ def grid_particle(elements,starting_size,n_atoms_added,n_hops,bond_score,het_sco
     dist = atoms.get_distances(center_id,range(len(atoms)))
 
     ids_ranked = np.arange(len(atoms))[dist.argsort()]
-
     # establish starting graph
     graph = edge_lib[np.all(np.isin(edge_lib[:],ids_ranked[:starting_size]),axis=1)]
 
     n_each_element = {e: n_atoms_added/len(elements) for e in elements}
     n_each_element = iteround.saferound(n_each_element, 0)
+    
+    n_each_charge = {c: n_atoms_added/len(charges) for c in charges}
+    n_each_charge = iteround.saferound(n_each_charge, 0)
+    charge_list = list(itertools.chain.from_iterable([[charge_idx] * int(n) for charge_idx, n in n_each_charge.items()]))
     # Shuffle list of surface element ids and set up 3D grid
     element_list = list(itertools.chain.from_iterable([[metal_idx] * int(n) for metal_idx, n in n_each_element.items()]))
     np.random.shuffle(element_list)
     for id in ids_ranked[:starting_size]:
         symbol_lib[id] = element_list.pop()
+        charge_lib[id] = charge_list.pop()
+        
 
     while len(element_list) > 0:
         # choice of element added
         rnd_symbol = element_list.pop()
-
+        rnd_charge = charge_list.pop()
         # all edges involving particle ids
         edges = edge_lib[np.isin(edge_lib[:,0],graph[:,0])]
 
@@ -84,7 +99,7 @@ def grid_particle(elements,starting_size,n_atoms_added,n_hops,bond_score,het_sco
         start_id = np.random.choice([id for id in rnk_by_bond.keys() if rnk_by_bond[id] > 2],1)
 
         candidates.append(start_id[0])
-        cand_score.append(score(start_id,avail_edges,rnd_symbol,symbol_lib, bond_score, het_score, hom_score))
+        cand_score.append(score(start_id,avail_edges,rnd_symbol,symbol_lib, bond_score, het_score, hom_score, rnd_charge, charge_lib, charge_score))
         # print(f"cand_score: {cand_score}")
         # hopping sequence
         for hop in range(n_hops):
@@ -104,16 +119,19 @@ def grid_particle(elements,starting_size,n_atoms_added,n_hops,bond_score,het_sco
         best_cand = candidates[np.argmax(cand_score)]
         # update symbol library
         symbol_lib[best_cand] = rnd_symbol
-
+        charge_lib[best_cand] = rnd_charge
         # add chosen candidate edges to particle graph
         graph = np.r_[graph,avail_edges[avail_edges[:, 1] == best_cand]]
         graph = np.r_[graph,avail_edges[avail_edges[:, 1] == best_cand][:,::-1]]
     # set symbols for the atoms object and delete ghost atoms
     atoms.set_chemical_symbols(symbol_lib)
-
     del atoms[[atom.index for atom in atoms if atom.symbol=='H']]
+    new_charge_lib = []
+    for charge in charge_lib:
+        if charge!='H':
+            new_charge_lib.append(charge)
     
-    return atoms
+    return atoms, np.array(new_charge_lib)
 
 def chi2_uncert(observed_frac,expected_frac,uncertainties):
         chi2 = np.sum((observed_frac-expected_frac)**2/uncertainties**2)
@@ -130,15 +148,17 @@ def pearsons_chi2(observed_N, expected_N):
 N_particles = 500
 kwarg_grid = {'elements': [sys.argv[1:]],#[elements[:i+2] for i in range(4)],
               'n_hops': range(8),
-              'het_mod': np.linspace(-0.75,0.75,13)}
+              'het_mod': np.linspace(-0.5,0.5,9),
+              'charge_sc': np.linspace(0,1,5),
+              'charge_n': [2,3]}
 
 for kwargs in ParameterGrid(kwarg_grid):
 
         bonds = np.array([set(a) for a in list(itertools.combinations_with_replacement(kwargs['elements'], 2))])
         pval_bootstrap = []
-
+        pval_charges = []
         for i in range(N_particles):
-            atoms = grid_particle(kwargs['elements'],13,250,kwargs['n_hops'],1.0,kwargs['het_mod'],0.0,i)
+            atoms, ch_list = grid_particle(kwargs['elements'],13,250,kwargs['n_hops'],1.0,kwargs['het_mod'],0.0,i,kwargs['charge_sc'],kwargs['charge_n'])
             #traj = Trajectory(f'traj/{len(kwargs["elements"])}_{kwargs["n_hops"]}_{kwargs["het_mod"]:.2f}_{str(i).zfill(4)}.traj',atoms=None, mode='w')
             #traj.write(atoms)
             ana_object = analysis.Analysis(atoms, bothways=False)
@@ -150,30 +170,51 @@ for kwargs in ParameterGrid(kwarg_grid):
 
             symbols = np.array(atoms.get_chemical_symbols())
 
+            ch_values= np.delete(np.linspace(-kwargs['charge_n'],kwargs['charge_n'],2*kwargs['charge_n']+1,kwargs['charge_n']), 2)
+            ch_bonds = np.array([set(a) for a in list(itertools.combinations_with_replacement(ch_values, 2))])
+            
+            ch_observed = np.zeros(len(ch_bonds))
             observed = np.zeros(len(bonds))
             for edge in all_edges:
                 observed[np.argwhere(set(symbols[edge]) == bonds)[0][0]] += 1
+                ch_observed[np.argwhere(set(ch_list[edge]) == ch_bonds)] += 1
+
 
             expected = []
             for bond in bonds:
                 sets = np.array([set(a) for a in list(itertools.product(kwargs['elements'], kwargs['elements']))])
                 expected.append(sum(bond == sets) / len(sets) * sum(observed))
 
+            ch_expected = []
+
+            for ch in ch_bonds:
+                sets = np.array([set(a) for a in list(itertools.product(ch_values, ch_values))])
+                ch_expected.append(sum(ch == sets) / len(sets) * sum(ch_observed))
+
             _, _, pval = pearsons_chi2(observed, expected)
             pval_bootstrap.append(pval)
+            _, _, ch_pval = pearsons_chi2(ch_observed, ch_expected)
+            pval_charges.append(ch_pval)
 
-        fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-        ax.hist(pval_bootstrap, bins=25, range=(0, 1), histtype='bar', color='steelblue', alpha=0.7)
-        ax.hist(pval_bootstrap, bins=25, range=(0, 1), histtype='step', color='steelblue')
-        ax.vlines(np.median(pval_bootstrap), 0, ax.get_ylim()[1], color='firebrick')
+        fig, ax = plt.subplots(1, 2, figsize=(8, 8),sharey=True,sharex=True)
+        #pval
+        ax[0].hist(pval_bootstrap, bins=25, range=(0, 1), histtype='bar', color='steelblue', alpha=0.7)
+        ax[0].hist(pval_bootstrap, bins=25, range=(0, 1), histtype='step', color='steelblue')
+        ax[0].vlines(np.median(pval_bootstrap), 0, ax.get_ylim()[1], color='firebrick')
+        
+        #ch
+        ax[1].hist(pval_bootstrap, bins=25, range=(0, 1), histtype='bar', color='steelblue', alpha=0.7)
+        ax[1].hist(pval_bootstrap, bins=25, range=(0, 1), histtype='step', color='steelblue')
+        ax[1].vlines(np.median(pval_bootstrap), 0, ax.get_ylim()[1], color='firebrick')
         ax.set(ylim=(0, ax.get_ylim()[1] * 1.2))
         ax.text(0.02, 0.98,r'N$_{elements}$: '+f'{len(kwargs["elements"])}'+'\n'+r'N$_{hops}$: '+f'{kwargs["n_hops"]}'+f'\nBond modifier: {kwargs["het_mod"]:.2f}' +\
-        f'\nMedian p-value = {np.median(pval_bootstrap):.2f} '+f'\nAdded atoms: ' + f'{kwargs["heanp_size"]}', family='monospace', fontsize=13, transform=ax.transAxes,verticalalignment='top')
+        f'\nMedian p-value = {np.median(pval_bootstrap):.2f} '+f'\Charge score and amount: ' + f'{kwargs["charge_sc"]}_{kwargs["charge_n"]}', family='monospace', fontsize=13, transform=ax.transAxes,verticalalignment='top')
         ax.set_xlabel(r"Pearson's $\chi^2$ p-value", fontsize=16)
         ax.set_ylabel('Frequency', fontsize=16)
-        fig.savefig(f'pvals/{len(kwargs["elements"])}_{kwargs["n_hops"]}_{kwargs["het_mod"]:.2f}_{kwargs["heanp_size"]}.png')
-        with open('grid.txt','a') as file:
-            file.write(f'{len(kwargs["elements"])},{kwargs["n_hops"]},{kwargs["het_mod"]:.2f},{np.median(pval_bootstrap):.2f},{kwargs["heanp_size"]}\n')
+
+        fig.savefig(f'pvalscharge/{len(kwargs["elements"])}_{kwargs["n_hops"]}_{kwargs["het_mod"]:.2f}__{kwargs["charge_sc"]}_{kwargs["charge_n"]}.png')
+        with open('gridcharge.txt','a') as file:
+            file.write(f'{len(kwargs["elements"])},{kwargs["n_hops"]},{kwargs["het_mod"]:.2f},{np.median(pval_bootstrap):.2f},{kwargs["charge_sc"]},{kwargs["charge_n"]}\n')
         plt.close()
 
 
