11ade323809bbf7c2b17c50545507bf35fcc4d01
BerlinObel <oscarobel@gmail.com>
2022-10-04 11:02:21 +0200

    plots

---
+++ plotcharge.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+file1 = open('gridcharge.txt', 'r')
+lines = file1.readlines()
+
+n_hops = range(8)
+het_mod = np.linspace(-0.5,0.5,9)
+charges = np.linspace(0,1,5)
+het_mod = [str(round(het,2)) for het in het_mod]
+pvals = np.zeros(shape=(len(charges),len(n_hops),len(het_mod)),dtype=float)
+pvals_ch2 = np.zeros(shape=(len(charges),len(n_hops),len(het_mod)),dtype=float)
+pvals_ch3 = np.zeros(shape=(len(charges),len(n_hops),len(het_mod)),dtype=float)
+Z =0
+X = -1
+old_ch = 0.0
+for line in lines:
+        ele, hop, mod, pval, charge_sc, charge_n, ch_pval  = line.split(",")
+        Y = int(hop)
+        if float(charge_sc) > old_ch:
+                old_ch = float(charge_sc)
+                Z += 1
+                X = -1
+        if Y == 0:
+                X += 1
+        pvals[Z,Y,X]=float(pval)
+        if int(charge_n) == 2:
+                pvals_ch2[Z,Y,X]=float(ch_pval)
+        else:
+                pvals_ch3[Z,Y,X]=float(ch_pval)
+        
+
+
+fig, ax = plt.subplots(5,3)
+ax[0,0].imshow(pvals[0,:,:])
+ax[0,1].imshow(pvals_ch2[0,:,:])
+ax[0,2].imshow(pvals_ch3[0,:,:])
+ax[1,0].imshow(pvals[1,:,:])
+ax[1,1].imshow(pvals_ch2[1,:,:])
+ax[1,2].imshow(pvals_ch3[1,:,:])
+ax[2,0].imshow(pvals[2,:,:])
+ax[2,1].imshow(pvals_ch2[2,:,:])
+ax[2,2].imshow(pvals_ch3[2,:,:])
+ax[3,0].imshow(pvals[3,:,:])
+ax[3,1].imshow(pvals_ch2[3,:,:])
+ax[3,2].imshow(pvals_ch3[3,:,:])
+ax[4,0].imshow(pvals[4,:,:])
+ax[4,1].imshow(pvals_ch2[4,:,:])
+ax[4,2].imshow(pvals_ch3[4,:,:])
+ax[0,0].set_title(f"Bond Pvals:")
+ax[0,1].set_title(f"4 charges:")
+ax[0,2].set_title(f"6 charges:")
+
+
+
+# Show all ticks and label them with the respective list entries
+#ax[0,0].set_xticks(np.arange(len(het_mod)), labels=het_mod)
+#ax[0,0].set_yticks(np.arange(len(n_hops)), labels=n_hops)
+
+# Rotate the tick labels and set their alignment.
+#plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+ #        rotation_mode="anchor")
+
+# Loop over data dimensions and create text annotations.
+
+
+plt.savefig("charges.png")
+plt.show()
+plt.close()
+
+# Loop over data dimensions and create text annotations.
+
