c7fbeab694ff9dfd46a0b7fd99bf5ee964f79f2f
BerlinObel <oscarobel@gmail.com>
2022-10-14 11:21:00 +0200

    Update separation.py

--- separation.py
+++ separation.py
@@ -126,6 +126,7 @@ for kwargs in ParameterGrid(kwarg_grid):
         bulk_chi = []
         outer_chi = []
         alt_outer = []
+        bulkouter = []
         if not path.isfile(f'chi_{len(kwargs["elements"])}_{kwargs["heanp_size"]}_edges.npy'):
             atoms = grid_particle(kwargs['elements'],5,kwargs['heanp_size'],0,1.0,0,0.0,1)
             view(atoms)
@@ -166,9 +167,10 @@ for kwargs in ParameterGrid(kwarg_grid):
             pval = chi2_square(observed, expected)
             pval_bootstrap.append(pval)
         """
-        for i in range(100000):
+        for i in range(5000):
             np.random.shuffle(symbols)
             outer_atoms = 0
+            oa_list = []
             observed = np.zeros((2,len(bonds)))
             for edge in all_edges:
                 if int(n_bonds[edge[0]]) == 12:
@@ -177,51 +179,85 @@ for kwargs in ParameterGrid(kwarg_grid):
                     observed[1,np.argwhere(set(symbols[edge]) == bonds)[0][0]] += 1  
                     outer_atoms += 1
 
+            """
+            print(np.average(observed[0,:]/observed[1,:]))
+            print(sum(observed[0,:])/sum(observed[1,:]))
+
+            r = np.cbrt((3/(4*np.pi))*kwargs['heanp_size'])
+            A = 4*np.pi*(r**2)
+            print(kwargs['heanp_size']/A)
+            """
+
+            both = np.zeros(len(bonds))
+            both += observed[0]+observed[1]
+            #print(both)
+
             bulk_chi.append(chi2(observed[0],np.array(expected)))
             outer_chi.append(chi2(observed[1],np.array(expected)))
             alt_outer.append(chi22(observed[1],np.array(expected),outer_atoms))
+            bulkouter.append(chi2(both,np.array(expected)))
             
-        """
+        
         mean_bulk = np.mean(bulk_chi)
-        mean_outer = np.mean(bulk_chi)
-        var = np.var(pval_bootstrap)
-        theta =  var/mean
-        k = mean/theta
-        X = np.linspace(0,50,1000)
-        Y = pdf(X,k,theta)
-
-        k2 = mean/2
-        Y2 = pdf(X,k2,2)
-        Y3 = pdf(X,7,2)
+        mean_outer = np.mean(outer_chi)
+        mean_comb = np.mean(bulkouter)
+        var_bulk = np.var(bulk_chi)
+        var_outer = np.var(outer_chi)
+        var_comb = np.var(bulkouter)
         """
+        t_b =  var_bulk/mean_bulk
+        t_o =  var_outer/mean_outer
+        t_c = var_comb/mean_comb
+        k_b = mean_bulk/t_b
+        k_o = mean_outer/t_o
+        k_c = mean_comb/t_c
+        X = np.linspace(-3.5,3.5,1000)
+        Y_b = pdf(X,k_b,t_b)
+        Y_o = pdf(X,k_o,t_o)
+        Y_c = pdf(X,k_c,t_c)
+        """
+        sigma_b = np.sqrt(var_bulk)
+        sigma_o = np.sqrt(var_outer)
+        sigma_c = np.sqrt(var_comb)
 
-        fig, ax = plt.subplots(1, 3, figsize=(8, 16))
-        ax[0].hist(bulk_chi, bins=10, histtype='bar', color='steelblue', alpha=0.7)
-        ax[0].hist(bulk_chi, bins=10, histtype='step', color='steelblue')
-        ax[1].hist(outer_chi, bins=10, histtype='bar', color='steelblue', alpha=0.7)
-        ax[1].hist(outer_chi, bins=10, histtype='step', color='steelblue')
-        ax[2].hist(outer_chi, bins=10, histtype='bar', color='steelblue', alpha=0.7)
-        ax[2].hist(outer_chi, bins=10, histtype='step', color='steelblue')
-        #ax2 = ax.twinx()
-        #ax2.plot(X,Y,color='seagreen')
-        #ax2.plot(X,Y2,'--',color='orange')
-        #ax2.plot(X,Y3,'-.',color='red')
+
+        X = np.linspace(-3.5,3.5,1000)
+        Y_b = stats.norm.pdf(X,mean_bulk,sigma_b)
+        Y_o = stats.norm.pdf(X,mean_outer,sigma_o)
+        Y_c = stats.norm.pdf(X,mean_comb,sigma_c)
+
+
+
+
+        fig, ax = plt.subplots(1, 3, figsize=(16, 8))
+        ax[0].hist(bulk_chi, bins=100, histtype='bar', color='steelblue', alpha=1)
+        ax[0].hist(bulkouter, bins=100, histtype='bar', color='orange', alpha=0.7)
+        ax[1].hist(bulk_chi, bins=100, histtype='bar', color='steelblue', alpha=1)
+        ax[1].hist(outer_chi, bins=100, histtype='step', color='orange', alpha=0.7)
+        #ax[2].hist(bulk_chi, bins=100, histtype='bar', color='steelblue', alpha=0.7)
+        
+        ax[2].plot(X,Y_b,color='seagreen')
+        ax[2].plot(X,Y_o,'--',color='orange')
+        ax[2].plot(X,Y_c,'-.',color='red')
         #ax.vlines(np.median(pval_bootstrap), 0, ax.get_ylim()[1], color='firebrick')
         #ax.vlines(np.percentile(pval_bootstrap,95),0, ax.get_ylim()[1], color='darkviolet')
         #ax.vlines(np.percentile(pval_bootstrap,99),0, ax.get_ylim()[1], color='seagreen')
         ax[0].set(ylim=(0, ax[0].get_ylim()[1] * 1.2))
         ax[1].set(ylim=(0, ax[1].get_ylim()[1] * 1.2))
-        ax[2].set(ylim=(0, ax[1].get_ylim()[1] * 1.2))
+        ax[2].set(ylim=(0, ax[2].get_ylim()[1] * 1.2))
         #ax2.set(ylim=(0, ax2.get_ylim()[1] * 1.2))
         #ax.text(0.02, 0.98,r'N$_{elements}$: '+f'{len(kwargs["elements"])}'+'\n'+r'N$_{hops}$: '+f'{kwargs["n_hops"]}'+f'\nBond modifier: {kwargs["het_mod"]:.2f}' +\
         #f'\nMedian p-value = {np.median(pval_bootstrap):.2f} '+f'\nAdded atoms: ' + f'{kwargs["heanp_size"]}', family='monospace', fontsize=13, transform=ax.transAxes,verticalalignment='top')
         #ax[0].text(0.02, 0.98,r'N$_{elements}$: '+f'{len(kwargs["elements"])}'+ f"\nNP size: {kwargs['heanp_size']}"+f'\nShape:{k}\n'+f'Scale:{theta}'+f'\nShape: {k2}'  , family='monospace', fontsize=13, transform=ax[0].transAxes,verticalalignment='top')
        
         #'\nMedian p-value = {np.median(pval_bootstrap):.2f}'+f"\n95%: {np.percentile(pval_bootstrap,95,method='inverted_cdf')}"+f"\n99%: {np.percentile(pval_bootstrap,99,method='inverted_cdf')}"
-        ax[0].set_xlabel(r"$\chi$" + " bulk", fontsize=16)
+        ax[0].set_xlabel(r"$\chi$" + " combined", fontsize=16)
         ax[1].set_xlabel(r"$\chi$" + " outer", fontsize=16)
-        ax[2].set_xlabel(r"$\chi$" + " outer alt", fontsize=16)
+        ax[2].set_xlabel(r"$\chi$" + " Normal dist", fontsize=16)
         ax[0].set_ylabel('Frequency', fontsize=16)
+        ax[0].legend(['Bulk_chi','combined'], loc='right')
+        ax[1].legend(['Bulk_chi','outer'], loc='right')
+        ax[2].legend(['Bulk','Outer','Combined'], loc='right')
         #ax2.set_ylabel('Probability', fontsize=16)
         #fig.savefig(f'pvals/{len(kwargs["elements"])}_{kwargs["n_hops"]}_{kwargs["het_mod"]:.2f}_{kwargs["heanp_size"]}.png')
         #fig.savefig(f'pvals/{len(kwargs["elements"])}_{kwargs["heanp_size"]}.png')
